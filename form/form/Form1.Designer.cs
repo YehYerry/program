f0f1c823130fc1ce7e08699ad9d284be75db58cb
YehYerry <49938703+YehYerry@users.noreply.github.com>
2020-06-01 14:38:21 +0800

    Add files via upload

---
+++ form/form/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_wait = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 400);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "傳送資料\r\n等待人數:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM6";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(9, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "收到的資料";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(269, 111);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(912, 506);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 111);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label_wait
+            // 
+            this.label_wait.AutoSize = true;
+            this.label_wait.Location = new System.Drawing.Point(148, 438);
+            this.label_wait.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_wait.Name = "label_wait";
+            this.label_wait.Size = new System.Drawing.Size(14, 15);
+            this.label_wait.TabIndex = 5;
+            this.label_wait.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1224, 75);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 634);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label_wait);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_wait;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ form/form/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace form
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort comport;
+        delegate void Display(Byte[] buffer);
+        private Int32 totalLength = 0;
+        byte[] array;
+        byte a = 0;
+        byte wait1 = 1;
+        byte wait2 = 0;        
+        int label = 0;
+        int countarray = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            comport = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+            comport.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            if (!comport.IsOpen)
+            {
+                comport.Open();
+            }
+            MessageBox.Show("開啟");*/
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            //byte wait1 = (byte)(a + 1);
+            if (wait1 == 10)
+            {
+                wait1 = 0;
+                wait2 = (byte)(wait2 + 1);
+            }
+            array = new byte[] { 0xED, 0xED, wait2, wait1, 0x00, 0x00, 0x03, 0x05, 0x01, 0x01, 0x00, 0x00 };
+            a = wait1;
+            wait1 += 1;
+            //comport.Write(array, 0, 12);
+            label += 1;
+            label_wait.Text = label.ToString();
+
+            foreach (byte byteValue in array)
+            {
+                Console.WriteLine(byteValue);
+            }
+        }
+       
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Byte[] buffer = new Byte[1024];
+            Int32 length = (sender as SerialPort).Read(buffer, 0, buffer.Length);
+            Array.Resize(ref buffer, length);
+            Display d = new Display(DisplayText);
+            this.Invoke(d, new Object[] { buffer });
+            comport.Write(array, 0, 12);
+
+            /*
+            Thread.Sleep(50);  //（毫秒）等待一定時間，確保資料的完整性 int len        
+            int len = comport.BytesToRead;
+            string receivedata = string.Empty;
+            if (len != 0)
+            {
+                byte[] buff = new byte[len];
+                comport.Read(buff, 0, len);
+                receivedata = Encoding.Default.GetString(buff);
+            }
+            MessageBox.Show(receivedata);
+            richTextBox1.AppendText(receivedata + "\r\n");
+            */
+        }
+
+        private void DisplayText(Byte[] buffer)
+        {
+            richTextBox1.Text += String.Format("{0}{1}", BitConverter.ToString(buffer), Environment.NewLine);
+            totalLength = totalLength + buffer.Length;
+            label1.Text = totalLength.ToString();
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            comport.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //byte[] array1 = { 0xED, 0xED, wait2, wait1, 0x00, 0x00, 0x03, 0x05, 0x01, 0x01, 0x00, 0x00 };
+            byte waits1 = wait1;
+            waits1 -= 1;
+           byte[] array1 = {  wait2, waits1 };
+           foreach (byte byteValue in array1)
+           {
+                    Console.WriteLine(byteValue);
+           }                      
+        }
+    }
+}
