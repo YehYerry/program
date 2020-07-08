c3abca878096c3d6065210ea079a7dd100c2eda8
jerry <s920030jerry@gmail.com>
2020-07-08 18:21:59 +0800

    company

--- testprint/testprint/Form1.Designer.cs
+++ testprint/testprint/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(285, 142);
+            this.button1.Location = new System.Drawing.Point(279, 140);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(210, 105);
@@ -75,11 +77,22 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(965, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.exit_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.mouseDoubleClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -87,6 +100,9 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.exit_Click);
+            this.DoubleClick += new System.EventHandler(this.mouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -97,6 +113,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- testprint/testprint/Form1.cs
+++ testprint/testprint/Form1.cs
@@ -18,10 +18,15 @@ namespace testprint
         string[] line = new string[1000];
         string[] line1 = new string[1000];
         int ctr,ctr1 = 0;
+        int pageH, pageW, pbw, pbh, photoW, photoH, wordstyle1, wordstyle2, wordstyle3, nums, numbw, numbh, str1s, str1bw, str1bh, str2s, str2bw, str2bh;
+        string numfont, str1font, str2font, text;
+        FontStyle wdstyle1, wdstyle2, wdstyle3;
+        DateTime doubleClickTimer = DateTime.Now;
+
         public Form1()
         {
             StreamReader str = new StreamReader(Application.StartupPath + @"\config.ifm");//讀取文字檔
-            StreamReader str1 = new StreamReader(Application.StartupPath + @"\print_config.txt");//讀取文字檔
+            StreamReader str1 = new StreamReader(Application.StartupPath + @"\print_config.txt");//讀取列印文字檔
             do
             {
                 ctr++; 
@@ -33,9 +38,30 @@ namespace testprint
 
             test = line[5];
             test1 = line1[2];
-
+            pageH = Convert.ToInt32(line1[2].Substring(6));
+            pageW = Convert.ToInt32(line1[3].Substring(7));
+            pbw = Convert.ToInt32(line1[6].Substring(12));
+            pbh = Convert.ToInt32(line1[7].Substring(13));
+            photoW = Convert.ToInt32(line1[8].Substring(6));
+            photoH = Convert.ToInt32(line1[9].Substring(7));
+            numfont = line1[12].Substring(7);
+            nums = Convert.ToInt32(line1[13].Substring(5));
+            wordstyle1 = Convert.ToInt32(line1[14].Substring(6));
+            numbw = Convert.ToInt32(line1[15].Substring(12));
+            numbh = Convert.ToInt32(line1[16].Substring(13));
+            str1font = line1[19].Substring(7);
+            str1s = Convert.ToInt32(line1[20].Substring(5));
+            wordstyle2 = Convert.ToInt32(line1[21].Substring(6));
+            str1bw = Convert.ToInt32(line1[22].Substring(12));
+            str1bh = Convert.ToInt32(line1[23].Substring(13));
+            str2font = line1[26].Substring(7);
+            str2s = Convert.ToInt32(line1[27].Substring(5));
+            wordstyle3 = Convert.ToInt32(line1[28].Substring(6));
+            str2bw = Convert.ToInt32(line1[29].Substring(12));
+            str2bh = Convert.ToInt32(line1[30].Substring(13));
+            text = line1[31].Substring(5);
             InitializeComponent();
-            String[] pairs = { "Color1=red", "Color2=green", "Color3=blue",
+            /*String[] pairs = { "Color1=red", "Color2=green", "Color3=blue",
                  "Title=Code Repository" };
             foreach (var pair in pairs)
             {
@@ -45,9 +71,8 @@ namespace testprint
                 Console.WriteLine("Key: {0}, Value: '{1}'",
                               pair.Substring(0, position),
                                pair.Substring(position + 1));
-            }
+            }*/
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             //宣告一個一表機
@@ -56,7 +81,7 @@ namespace testprint
             Margins margin = new Margins(0, 0, 0, 0);
             pd.DefaultPageSettings.Margins = margin;
             ////設定紙張大小 'vbPRPSUser'為使用者自訂
-            PaperSize pageSize = new PaperSize("vbPRPSUser", 256, 256);
+            PaperSize pageSize = new PaperSize("vbPRPSUser", pageH, pageW);//256
             pd.DefaultPageSettings.PaperSize = pageSize;
             //印表機事件設定
             pd.PrintPage += new PrintPageEventHandler(this.printCoupon_PrintPage);
@@ -75,6 +100,7 @@ namespace testprint
                 pd.PrintController.OnEndPrint(pd, new PrintEventArgs());
             }
         }
+
         private void printCoupon_PrintPage(object sender, PrintPageEventArgs e)
         {
             string str = System.Windows.Forms.Application.StartupPath;
@@ -84,11 +110,11 @@ namespace testprint
             //GetResultIntoImage(ref temp);//
 
             //設定圖片列印的x,y座標
-            int x = 20;   //e.MarginBounds.X
-            int y = 0;  //e.MarginBounds.Y
+            int x = pbw;   //e.MarginBounds.X; 20
+            int y = pbh;  //e.MarginBounds.Y; 0
             //圖片列印的大小
-            int width = 220;//temp.Width;
-            int height = 80;//temp.Height;
+            int width = photoW;//temp.Width; 220
+            int height = photoH;//temp.Height; 80
             Rectangle destRect = new Rectangle(x, y, width, height);
 
             // public void DrawImage (
@@ -103,13 +129,66 @@ namespace testprint
             //)
             //將圖片放入要列印的文件中
             e.Graphics.DrawImage(temp, destRect, 0, 0, temp.Width, temp.Height, System.Drawing.GraphicsUnit.Pixel);
-
+            switch (wordstyle1)
+            {
+                case 0:
+                    wdstyle1 = FontStyle.Regular;
+                    break;
+                case 1:
+                    wdstyle1 = FontStyle.Bold;
+                    break;
+                case 2:
+                    wdstyle1 = FontStyle.Italic;
+                    break;
+                case 3:
+                    wdstyle1 = FontStyle.Strikeout;
+                    break;
+                case 4:
+                    wdstyle1 = FontStyle.Underline;
+                    break;
+            }
+            switch (wordstyle2)
+            {
+                case 0:
+                    wdstyle2 = FontStyle.Regular;
+                    break;
+                case 1:
+                    wdstyle2 = FontStyle.Bold;
+                    break;
+                case 2:
+                    wdstyle2 = FontStyle.Italic;
+                    break;
+                case 3:
+                    wdstyle2 = FontStyle.Strikeout;
+                    break;
+                case 4:
+                    wdstyle2 = FontStyle.Underline;
+                    break;
+            }
+            switch (wordstyle3)
+            {
+                case 0:
+                    wdstyle3 = FontStyle.Regular;
+                    break;
+                case 1:
+                    wdstyle3 = FontStyle.Bold;
+                    break;
+                case 2:
+                    wdstyle3 = FontStyle.Italic;
+                    break;
+                case 3:
+                    wdstyle3 = FontStyle.Strikeout;
+                    break;
+                case 4:
+                    wdstyle3 = FontStyle.Underline;
+                    break;
+            }
             //列印文字
             Graphics MyGraphics = e.Graphics;
             SolidBrush MyBrush = new SolidBrush(Color.Black);
-            Font MyFont = new Font("標楷體", 20);//設定字型與大小
-            Font MyFont1 = new Font("標楷體", 10);
-            Font num = new Font("標楷體", 40);
+            Font num = new Font(numfont, nums, wdstyle1);//40
+            Font MyFont = new Font(str1font, str1s, wdstyle2);//設定字型與大小; 20
+            Font MyFont1 = new Font(str2font, str2s, wdstyle3);//10            
             float leftMargin = e.MarginBounds.Left;//取得文件左邊界
             float topMargin = e.MarginBounds.Top;//取得文件上邊界
             int count = 10;//起始列印的行數
@@ -125,13 +204,12 @@ namespace testprint
             // StringFormat format StringFormat，指定套用到所繪製文字的格式化屬性，例如，行距和對齊。
             //)
             //將要列印的文字放入要列印的文件中
-            MyGraphics.DrawString("0017", num, MyBrush, 70, 85, new StringFormat());
-            MyGraphics.DrawString("日期:" + DateTime.Now.ToString("yyyy/MM/dd"), MyFont, MyBrush, 30, 145, new StringFormat());
-            MyGraphics.DrawString("時間:" + DateTime.Now.ToString("HH:mm:ss"), MyFont, MyBrush, 30, 175, new StringFormat());
-            MyGraphics.DrawString("請依叫號 等候辦理", MyFont1, MyBrush, 80, 210, new StringFormat());
+            MyGraphics.DrawString("0017", num, MyBrush, numbw, numbh, new StringFormat()); //70,85
+            MyGraphics.DrawString("日期:" + DateTime.Now.ToString("yyyy/MM/dd"), MyFont, MyBrush, str1bw, str1bh, new StringFormat());//30, 145
+            MyGraphics.DrawString("時間:" + DateTime.Now.ToString("HH:mm:ss"), MyFont, MyBrush, str1bw, str1bh+30, new StringFormat());//30, 175
+            MyGraphics.DrawString( text , MyFont1, MyBrush, str2bw, str2bh, new StringFormat());//80, 210
 
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             string str = System.Windows.Forms.Application.StartupPath;
@@ -151,6 +229,20 @@ namespace testprint
         private void button3_Click(object sender, EventArgs e)
         {
             Console.WriteLine(test);
+        }
+        void mouseDoubleClick(object sender, EventArgs e)
+        {
+            doubleClickTimer = DateTime.Now; //記下DoubleClick的時間 
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            TimeSpan t = (TimeSpan)(DateTime.Now - doubleClickTimer); //DoubleClick後又點了一下, 計算時間差 
+
+            if (t.TotalMilliseconds <= 200) //如果小於200豪秒就執行 
+            {
+                Console.WriteLine("3");
+            } 
         }
     }    
 }
