3ef20236e63abdef30d8318cd196fb8b7be6956a
jerry <s920030@kimo.com>
2020-08-25 00:54:25 +0800

    home

--- clock/clock/comportshow.Designer.cs
+++ clock/clock/comportshow.Designer.cs
@@ -39,11 +39,11 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(292, 149);
+            this.button1.Location = new System.Drawing.Point(104, 152);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 79);
+            this.button1.Size = new System.Drawing.Size(121, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "確認COMPORT並離開";
+            this.button1.Text = "儲存並離開";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -61,22 +61,22 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(288, 69);
+            this.label1.Location = new System.Drawing.Point(280, 72);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 21);
+            this.label1.Size = new System.Drawing.Size(189, 21);
             this.label1.TabIndex = 4;
-            this.label1.Text = "未開啟COMPORT";
+            this.label1.Text = "選擇開啟COMPORT";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(78, 149);
+            this.button2.Location = new System.Drawing.Point(322, 152);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 79);
+            this.button2.Size = new System.Drawing.Size(111, 58);
             this.button2.TabIndex = 5;
-            this.button2.Text = "測試開啟";
+            this.button2.Text = "程式縮小化";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
